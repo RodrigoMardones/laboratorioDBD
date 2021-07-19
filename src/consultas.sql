0b85d7a8260366648713a8fa7d96e39eaefdddf4
Rodrigo Mardones A <rignmardones@falabella.cl>
2021-07-19 00:23:08 -0400

    modificacion

--- src/consultas.sql
+++ src/consultas.sql
@@ -2,5 +2,7 @@
 /*
 pagos ultimos 6 meses
 */
-select * from intencion where modificacion > now() - interval '6 months';
-
+select * from intencion where
+(fk_id_persona = 1 and
+ estado = 'paid' and
+ modificacion > now() - interval '6 months');
